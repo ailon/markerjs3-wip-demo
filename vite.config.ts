2c80cbf41e4e59b70d721133fcfb21a74d63876e
ailon <ailon@ailon.org>
2025-03-14 13:02:41 +0200

    swap postcss tailwind plugin to vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3333,
   },
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
 });
